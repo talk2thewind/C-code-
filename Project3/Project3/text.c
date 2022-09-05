a0d7518cdfadf007d2d3b97d64a3cc41df2e8a11
talk2thewind <2542552085@qq.com>
2022-09-05 15:15:58 +0800

    变量的作用域及其生命周期 extern函数跨源文件声明（注意后缀一致 局部变量生命周期为大括号内（代码一行行运行 全局变量生命周期即为程序生命周期

---
+++ Project3/Project3/text.c
@@ -0,0 +1,4 @@
+#define _CRT_SECURE_NO_WARNINGS
+
+	int g_val = 2022;
+	
---
+++ Project3/Project3/源.c
@@ -0,0 +1,8 @@
+#define _CRT_SECURE_NO_WARNINGS
+#include <stdio.h>
+extern int g_val;
+int main()
+{
+	printf("g_val=%d\n",g_val);
+	return 0;
+}
