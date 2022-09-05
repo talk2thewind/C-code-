42633fa4818a99b674fdb8b4c7b7bfe00ad914c3
talk2thewind <2542552085@qq.com>
2022-09-05 10:32:28 +0800

    第二天 数据类型以及变量

---
+++ Project2/Project2/源.cpp
@@ -0,0 +1,46 @@
+#define _CRT_SECURE_NO_WARNINGS
+
+//#include<stdio.h>
+//int main()
+//{
+//	int age = 18;
+//	double weight = 65.3;
+//	age = age + 1;
+//	weight = weight - 10;
+//	printf("%d\n", age);
+//	printf("%lf\n", weight);
+//	return 0;
+//}
+//#include<stdio.h>
+//int a = 100;
+//int main()
+//{
+//	printf("%d", a);
+//	return 0;
+//}
+#include<stdio.h>
+//int main()
+//{
+//	int a = 1;
+//	printf("%d", a);
+//	return 0;
+//}
+//int global = 2019;
+//int main()
+//{
+//	int local = 2018;
+//	int global = 2020;
+//	printf("global=%d\n", global);
+//	return 0;
+//}
+int main()
+{
+	int a = 0;
+	int b = 0;
+	int sum = 0;
+	scanf("%d %d", &a,&b);
+	sum = a + b;
+	printf("%d", sum);
+		return 0;
+
+}
