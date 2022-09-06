2929cb9697a489cae2c9cdfe08d73cd7b1d5585b
talk2thewind <2542552085@qq.com>
2022-09-06 15:02:57 +0800

    字符串： 字符串为双引号圈住的一串字符，单引号圈的为字符 字符数组char arr[]=”“与char arr []={}不一样 \0为字符串结束的标志 打印字符串用%s 求字符串长度函数为strlen（）；（可能需要引用#include<string.h> 大括号引用的字符串无\0的，长度为随机值
    转义字符：
    三字母词？？）➡】
    \'为打印单引号
    \“为打印双引号
    printf打印格式问题？
    \\为打印双斜杠
    \a发出警告声
    %c打印字符
    %s打印字符串
    %d打印整形
    \ddd为八进制（不能出现8与9
    \xdd为两位数十六进制
    打印单个字符时输数字变为ascii表中符号
    注意区分双引号与单引号

---
+++ Project5_9_6/Project5_9_6/源.c
@@ -0,0 +1,27 @@
+#define _CRT_SECURE_NO_WARNINGS
+#include<stdio.h>;
+//int main()
+//{
+//	/*char arr[]="hello";*/
+//	char arr1[] = "abc";
+//	char arr2[] = { 'a','b','c' };
+//	//printf("%s\n", arr1);
+//	//printf("%s", arr2);
+//	/*int len = strlen("hello");
+//	printf("%d\n", len);*/
+//	
+//	printf("%d\n", strlen(arr1));
+//	printf("%d\n", strlen(arr2));
+//
+//	return 0;
+//}
+int main()
+{
+	//printf("\?\?\?)");
+	//printf("%c\n",'\'');
+	//printf("\a \a \a");
+	/*printf("%s", "'");*/
+	//printf("%c\n", '\x30');//±ØÐëµ¥ÒýºÅ
+	printf("%c\n", '\327');
+	return 0;
+}
